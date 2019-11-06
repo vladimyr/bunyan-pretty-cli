ccc6579ab6da3d4b49d90b451d7b82493ca97fce
Dario Vladovic <d.vladimyr@gmail.com>
2019-11-06 13:10:17 +0100

    Move rewire specific logic to separate module

---
+++ bunyan.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const bunyan = require('./rewire')('bunyan/bin/bunyan');
+
+const getter = (proxiedModule, name) => ({
+  get: () => proxiedModule.__get__(name)
+});
+
+const prop = (proxiedModule, name) => ({
+  ...getter(proxiedModule, name),
+  set: val => proxiedModule.__set__(name, val)
+});
+
+module.exports = Object.defineProperties({}, {
+  emit: prop(bunyan, 'emit'),
+  emitRecord: prop(bunyan, 'emitRecord'),
+  indent: getter(bunyan, 'indent'),
+  isValidRecord: getter(bunyan, 'isValidRecord'),
+  levelFromName: getter(bunyan, 'levelFromName'),
+  main: getter(bunyan, 'main'),
+  OM_FROM_NAME: getter(bunyan, 'OM_FROM_NAME'),
+  OM_LONG: getter(bunyan, 'OM_LONG'),
+  OM_SHORT: getter(bunyan, 'OM_SHORT'),
+  parseArgv: prop(bunyan, 'parseArgv'),
+  print: prop(bunyan, 'p'),
+  printHelp: prop(bunyan, 'printHelp')
+});
--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 'use strict';
 
 const args = require('minimist')(process.argv.slice(2));
-const bunyan = require('./rewire')('bunyan/bin/bunyan');
+const bunyan = require('./bunyan');
 const dargs = require('dargs');
 const dedent = require('dedent');
 const JoyCon = require('joycon');
@@ -29,22 +29,14 @@ joycon.addLoader({
   loadSync: path => parseJSON(readFileSync(path, 'utf-8'))
 });
 
-const OM_LONG = bunyan.__get__('OM_LONG');
-const OM_SHORT = bunyan.__get__('OM_SHORT');
-const OM_FROM_NAME = bunyan.__get__('OM_FROM_NAME');
-const levelFromName = bunyan.__get__('levelFromName');
+const { emitRecord: _emitRecord } = bunyan;
+bunyan.emitRecord = emitRecord;
 
-const indent = bunyan.__get__('indent');
-const isValidRecord = bunyan.__get__('isValidRecord');
-const emit = bunyan.__get__('emit');
-const print = bunyan.__get__('p');
+const { parseArgv: _parseArgv } = bunyan;
+bunyan.parseArgv = parseArgv;
 
-const _emitRecord = bunyan.__get__('emitRecord');
-const _parseArgv = bunyan.__get__('parseArgv');
-const _printHelp = bunyan.__get__('printHelp');
-bunyan.__set__('emitRecord', emitRecord);
-bunyan.__set__('parseArgv', parseArgv);
-bunyan.__set__('printHelp', printHelp);
+const { printHelp: _printHelp } = bunyan;
+bunyan.printHelp = printHelp;
 
 main(args);
 
@@ -52,10 +44,11 @@ function main({ config, ...args }) {
   const argv = process.argv.slice(0);
   argv.length = 2;
   argv.push(...dargs(args));
-  bunyan.__get__('main')(argv);
+  bunyan.main(argv);
 }
 
 function parseArgv(input) {
+  const { levelFromName, OM_FROM_NAME } = bunyan;
   const options = _parseArgv.call(this, input);
   const config = loadConfig(args.config);
   if (config.level) config.level = levelFromName[config.level.toLowerCase()];
@@ -65,7 +58,8 @@ function parseArgv(input) {
 
 function printHelp() {
   let help = '';
-  bunyan.__set__('p', input => (help += '\n' + input));
+  const { print: _print } = bunyan;
+  bunyan.print = input => (help += '\n' + input);
   _printHelp.call(this);
   help = help.trimLeft();
   help = help.replace(/General options:/, header => dedent`
@@ -76,11 +70,12 @@ function printHelp() {
                     not specified
   `);
   help = help.replace(/bunyan(\s)/g, 'bunyan-pretty$1');
-  bunyan.__set__('p', print);
-  print(help);
+  bunyan.print = _print;
+  _print(help);
 }
 
 function emitRecord(record, line, options, stylize) {
+  const { emit: _emit, indent, isValidRecord, OM_LONG, OM_SHORT } = bunyan;
   const { outputMode, customPrettifiers = {} } = options;
   if (![OM_LONG, OM_SHORT].includes(outputMode) || !isValidRecord(record)) {
     return _emitRecord.apply(this, arguments);
@@ -94,9 +89,9 @@ function emitRecord(record, line, options, stylize) {
     extras += indent(`${key}: ${customPrettifier(value, key, record)}`) + '\n';
     return acc;
   }, {});
-  bunyan.__set__('emit', input => emit(input + extras));
+  bunyan.emit = input => _emit(input + extras);
   const result = _emitRecord.call(this, newRecord, line, options, stylize);
-  bunyan.__set__('emit', emit);
+  bunyan.emit = _emit;
   return result;
 }
 
